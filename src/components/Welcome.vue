640a08144bef6e42a61e4cd5754ed13c295f9aee
2404012458 <24040124588@qq.com>
2020-09-22 09:36:03 +0800

    Initial commit

---
+++ src/components/Welcome.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="main_welcome">
+        <h3>欢迎来到bilibili后台管理</h3>
+    </div>
+</template>
+<style lang="less" scoped>
+
+</style>
+<script>
+export default {
+    
+}
+</script>
---
+++ src/components/datamag.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "datamag"
+    }
+</script>
+
+<style scoped>
+
+</style>
